ca000a5d1051e24b87014b019bddd759c43b3eb1
Roberto <vannucchi.roberto@gmail.com>
2026-09-16 10:43:11 -0300

    Setup Program.cs.

---
+++ src/OrderFlow.Api/Configurations/ConfigRabbitMq.cs
@@ -0,0 +1,25 @@
+using Wolverine;
+using Wolverine.RabbitMQ;
+using OrderFlow.Application.Commands;
+using OrderFlow.Infrastructure.Persistence;
+
+namespace OrderFlow.Api.Configurations;
+
+public static class ConfigRabbitMq
+{
+    public static void ConfigureRabbitMq(this WebApplicationBuilder builder)
+    {
+        var rabbitMqConnectionString = builder.Configuration.GetConnectionString("RabbitMq");
+        if  (string.IsNullOrWhiteSpace(rabbitMqConnectionString))
+            throw new InvalidOperationException("RabbitMq connection string not set");
+
+        builder.Host.UseWolverine(opts =>
+        {
+            opts.Discovery.IncludeAssembly(typeof(CreateOrderCommand).Assembly);
+            opts.CodeGeneration.AlwaysUseServiceLocationFor<OrderFlowDbContext>();
+            opts.UseRabbitMq(rabbitMqConnectionString).AutoProvision();
+            opts.PublishMessage<OrderCreatedEvent>().ToRabbitQueue("order-created");
+            opts.ListenToRabbitQueue("order-created");
+        });
+    }
+}
---
+++ src/OrderFlow.Api/Configurations/MapApiRoutes.cs
@@ -0,0 +1,36 @@
+using Mapster;
+using OrderFlow.Application.Commands;
+using OrderFlow.Application.Queries;
+using OrderFlow.Infrastructure.Enums;
+using Wolverine;
+
+namespace OrderFlow.Api.Configurations;
+
+public record CreateOrderRequest(string CustomerName, decimal TotalAmount);
+
+public static class MapApiRoutes
+{
+    public static void MapApisRoutes(this WebApplication app)
+    {
+        app.MapPost("/orders", async (CreateOrderRequest request, IMessageBus messageBus) =>
+        {
+            var command = request.Adapt<CreateOrderCommand>();
+            var orderId = await messageBus.InvokeAsync<Guid>(command);
+            return Results.Created($"/orders/{orderId}", new { id = orderId });
+        });
+
+        app.MapGet("/orders/{id:guid}", async (Guid id, IMessageBus messageBus) =>
+        {
+            var query = new GetOrderQuery(id);
+            var order = await messageBus.InvokeAsync<OrderResponse?>(query);
+            return order == null ? Results.NotFound() : Results.Ok(order);
+        });
+
+        app.MapGet("/orders", async (OrderStatus status, IMessageBus messageBus) =>
+        {
+            var query = new GetOrdersByStatusQuery(status);
+            var orders = await messageBus.InvokeAsync<List<OrderResponse>>(query);
+            return Results.Ok(orders);
+        });
+    }
+}
